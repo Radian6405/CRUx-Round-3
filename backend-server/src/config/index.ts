43537170ed0b4250fa60dc1a3afa28acd73f0014
Sai Vardhan <vardhan.6405@gmail.com>
2024-05-29 15:27:31 +0530

    build: expressjs setup, mongoDB connection

---
+++ backend-server/src/config/index.ts
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import { mongo } from 'mongoose';
+
+dotenv.config();
+
+const MONGO_USERNAME: string = process.env.MONGO_USERNAME || '';
+const MONGO_PASSWORD: string = process.env.MONGO_PASSWORD || '';
+
+const MONGO_URL:string = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@localhost:27017/bitsbidsdb`;
+
+const PORT:number = process.env.SERVER_PORT ? Number(process.env.SERVER_PORT) : 8000;
+
+export const config = {
+    mongo: {
+        url: MONGO_URL
+    },
+    server: {
+        port: PORT
+    }
+}
