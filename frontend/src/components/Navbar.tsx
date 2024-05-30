4455a3dc12c473da865bc20dec44adcbf1f5dc63
Sai Vardhan <vardhan.6405@gmail.com>
2024-05-30 17:34:31 +0530

    feat: Navbar component

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,25 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar sx={{ justifyContent: "center" }}>
+          <Link to={"/"}>
+            <Button color="inherit">Auctions</Button>
+          </Link>
+          <Link to={"/rooms"}>
+            <Button color="inherit">Rooms</Button>
+          </Link>
+          <Link to={"/create"}>
+            <Button color="inherit">Create</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
