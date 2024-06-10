f742cea57ed626cc0d18c3acfc98740d00f4e1f3
Sai Vardhan <vardhan.6405@gmail.com>
2024-06-10 13:44:20 +0530

    feat: New auctions in a room now send a notice mail to all its users

--- backend-server/src/routes/auctions.ts
+++ backend-server/src/routes/auctions.ts
@@ -6,6 +6,10 @@ import { ObjectId } from "mongoose";
 import { authenticateToken } from "../utils/authHelpers";
 import { upload, uploadToCloudinary } from "../utils/cloudinary/fileFunctions";
 
+import dotenv from "dotenv";
+import { sendMail } from "../utils/nodemailer/mailService";
+dotenv.config();
+
 const router: Router = Router();
 
 router.get(
@@ -99,8 +103,12 @@ router.post(
     let newID;
     try {
       if (req.body.room !== null) {
+        let mailList = [];
         // if auction is private
-        const findRoom = await Room.findOne({ name: req.body.room });
+        const findRoom = await Room.findOne({ name: req.body.room })
+          .populate("creator", "email")
+          .populate("admins", "email")
+          .populate("members", "email");
         if (findRoom === null) return res.status(404).send("room not found");
 
         const newAuction = new Auction({
@@ -112,8 +120,25 @@ router.post(
 
         findRoom.auctionCount = parseInt(String(findRoom.auctionCount)) + 1;
         const saveRoom = await findRoom.save();
-
         const savedAuction = await newAuction.save();
+
+        const creator: any = findRoom.creator;
+        mailList.push(String(creator.email));
+        const admins: any = findRoom.admins;
+        admins.forEach((element: { email: string }) => {
+          mailList.push(element.email);
+        });
+        const members: any = findRoom.members;
+        members.forEach((element: { email: string }) => {
+          mailList.push(element.email);
+        });
+
+        sendAuctionNoticeMail(
+          mailList,
+          String(savedAuction._id),
+          String(findRoom.name),
+          String(savedAuction.title)
+        );
         newID = savedAuction._id;
       } else {
         // if auction is public
@@ -125,6 +150,7 @@ router.post(
         const savedAuction = await newAuction.save();
         newID = savedAuction._id;
       }
+
       res.status(201).send({
         msg: "Sucessfully created auction. Redirecting shortly...",
         id: newID,
@@ -159,5 +185,28 @@ router.post(
     }
   }
 );
+
+async function sendAuctionNoticeMail(
+  mailList: string[],
+  id: string,
+  roomName: string,
+  auctionName: string
+) {
+  const from: string = String(process.env.MAIL_USERNAME);
+  const subject: string = `New Acution in ${roomName} `;
+
+  const auctionLink: string = `http://127.0.0.1:5173/auctions/${id}`;
+  const mailTemplate: string = `<p>
+There has been a new auction for ${auctionName}<br/>
+Click below to view<br/>
+${auctionLink}<br/>
+If you have problems, please paste the above URL into your web browser.<br/>
+</p>`;
+
+  mailList.forEach(async (mail) => {
+    const to: string = mail;
+    const mailResponse = await sendMail(from, to, subject, mailTemplate);
+  });
+}
 
 export default router;
